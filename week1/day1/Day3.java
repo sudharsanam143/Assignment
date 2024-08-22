42b7e0ca969c2bd43b30c629154fa0b33537103f
sudharsanam143 <117083943+sudharsanam143@users.noreply.github.com>
2024-08-22 18:55:27 +0530

    Add files via upload
    week 1 assignment

---
+++ week1/day1/Day3.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Day3 {
+
+	public static void main(String[] args) {
+		String mobileName = "Realme";
+		String mobileModel = "realme13";
+		int mobilePrice = 25000;
+		Long mobileIminumber = 122543536363773l;
+		float mobileOffer = 8.0000f;
+		char mobileLogo = 'R';
+		boolean isDisplayed = true;
+		System.out.println(mobileName);
+		System.out.println(mobilePrice);
+		System.out.println(mobileIminumber);
+		System.out.println(mobileOffer);
+		System.out.println(mobileLogo);
+		System.out.println(isDisplayed);
+		System.out.println(mobileName + " " + mobilePrice + " " + mobileIminumber + " " + mobileOffer + " " + mobileLogo
+				+ " " + isDisplayed);
+		System.out.println(mobileName + " \n" + mobilePrice + " \n" + mobileIminumber + "\n " + mobileOffer + "\n "
+				+ mobileLogo + " \n" + isDisplayed);
+
+	}
+
+}
