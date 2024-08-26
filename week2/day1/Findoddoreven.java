217c41273ce620fd53d0dc617f42cb5b9f0fb332
sudharsanam143 <117083943+sudharsanam143@users.noreply.github.com>
2024-08-26 19:46:29 +0530

    Add files via upload

---
+++ week2/day1/Findoddoreven.java
@@ -0,0 +1,17 @@
+package week2.day1;
+
+public class Findoddoreven {
+
+	public static void main(String[] args) {
+		int n=143;
+		if (n%2!=0) {
+			System.out.println("It is odd Number");
+			
+		} else {
+			System.out.println("It is Even number");
+
+		}
+		}
+
+}
+//It is odd Number
---
+++ week2/day1/Oddnumber.java
@@ -0,0 +1,31 @@
+package week2.day1;
+
+public class Oddnumber {
+
+	public static void main(String[] args) {
+		for(int i=0;i<33;i++) {
+			if (i%2!=0) {
+				System.out.println(i);
+				
+			} 
+			}
+			
+		}
+	}
+
+//1
+//3
+//5
+//7
+//9
+//11
+//13
+//15
+//17
+//19
+//21
+//23
+//25
+//27
+//29
+//31
